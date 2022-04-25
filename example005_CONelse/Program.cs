4c26046fcafefdcc99f651a84ba6639c44c19510
ruslan <rus.pro94@bk.ru>
2022-04-25 19:35:33 +0500

    Example 005

---
+++ example005_CONelse/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите имя пользователя пожалуйста: ") ;
+string username = Console.ReadLine() ;
+
+if(username == "Руслан") ;
+{
+     Console.WriteLine("Ура, это же Руслан") ;
+}
+else
+{
+     Console.Write("СаламБрат, ") ;
+     Console.WriteLine(username) ;
+}
+     
+
+    
+    
+   
